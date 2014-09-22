1112a7e634661fda00ddc16ccc2efed65c66aa03
iirelu <totallymyrealemail@hush.ai>
2014-09-22 03:52:24 +0100

    Revbuild 0.1

---
+++ revbuild.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require_relative 'coords.rb'
+
+def revget(coord)
+  #`curl http://www.mateuszskutnik.com/submachine/subnet_data/#{coord}.swf -sr 4-8`.unpack("L<")[0]
+  123
+end
+
+responses = {}
+revs = {}
+
+COORDS.each do |coord,name|
+  responses[coord] = IO.popen("curl http://www.mateuszskutnik.com/submachine/subnet_data/#{coord}.swf -sr 4-8")
+end
+
+responses.each do |coord, response|
+  revs[coord] = response.readlines[0].unpack("L<")[0]
+end
+
+output = '{| class="article-table sortable"
+!Name
+!data-sort-type="text"|Coords
+!data-sort-type="number"|REV size
+|-
+'
+
+COORDS.each do |coord,name|
+  output += "|[[#{name}]]\n"
+  output += "|#{coord}\n"
+  output += "|#{revs[coord]}\n"
+  output += "|-\n"
+end
+
+output += "|}"
+
+puts output
